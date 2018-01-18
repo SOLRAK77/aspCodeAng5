a19d1c0bf2c59db9abcd75f8f8f313243023af04
carlos-linux <ccastillo.perez77@gmail.com>
2018-01-18 01:51:57 -0600

    CL-17 se agrego CORS

--- Startup.cs
+++ Startup.cs
@@ -22,6 +22,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContextApp>(x => x.UseSqlServer(Configuration.GetConnectionString("DefaultConn")));
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -31,7 +32,8 @@ namespace DatingApp.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
+                      
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
             app.UseMvc();
         }
     }
